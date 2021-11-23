a12d2542ecde76158cb2b20b09eadfd2730d794f
makaulucky <makaulucky20@gmail.com>
2021-11-23 16:25:38 +0300

    Initial commit

---
+++ include/sidebar.php
@@ -0,0 +1,21 @@
+<?php
+error_reporting(0);
+ob_start();
+session_start();
+?>
+<div>
+    <div class="sidebar">
+        <div class="sidebar-header">
+            <h3>Address Book</h3>
+        </div>
+        <div class="sidebar-body">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="add.php">Add Contact</a></li>
+                <li><a href="edit.php">Edit Contact</a></li>
+                <li><a href="delete.php">Delete Contact</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
